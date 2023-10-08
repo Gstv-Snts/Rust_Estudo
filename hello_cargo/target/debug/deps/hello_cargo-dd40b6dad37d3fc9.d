34aea4b12994669628e63abd08b6c206f79c0cb4
Gustavo <gustavosantossilveira12360@gmail.com>
2023-10-07 23:40:50 -0300

    Hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-dd40b6dad37d3fc9.d
@@ -0,0 +1,5 @@
+/home/gustavo/Documents/Rust_Estudo/hello_cargo/target/debug/deps/hello_cargo-dd40b6dad37d3fc9.rmeta: src/main.rs
+
+/home/gustavo/Documents/Rust_Estudo/hello_cargo/target/debug/deps/hello_cargo-dd40b6dad37d3fc9.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/gustavo/Documents/Rust_Estudo/hello_cargo/target/debug/hello_cargo: /home/gustavo/Documents/Rust_Estudo/hello_cargo/src/main.rs
